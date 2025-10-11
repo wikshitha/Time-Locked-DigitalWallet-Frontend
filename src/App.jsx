d2ffc5c2cf714df671527ce54f27c1fc4546ae21
Wikshitha Umindu <uminduwikshitha@gmail.com>
2025-10-11 13:01:33 +0530

    Frontend settup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🔐 Time-Locked Digital Will & Asset Handover System</h1>
+      <p>Frontend setup is ready 🚀</p>
+    </div>
+  );
+}
+
+export default App;
